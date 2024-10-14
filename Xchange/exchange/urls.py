268468ab7bb114910bf27902f8a6a3e2514545f9
Yonatan <107502940+Biyuktul@users.noreply.github.com>
2024-10-14 09:58:42 +0300

    Delete Xchange/exchange directory

--- Xchange/exchange/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from .api import hi_api, run_scraper_view
-
-
-urlpatterns = [
-    path('api/', hi_api, name='hi_api'),
-    path('run-scraper/', run_scraper_view, name='run_scraper'),
-]
